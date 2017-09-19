9c87440277c465d282f98552bbf6548a10d1530e
ferrwan <ferrwanz@gmail.com>
2017-09-20 02:21:57 +0700

    fix #3

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ const _ = require('lodash');
 const Telebot = require('telebot')
 const fs = require('fs')
 const log = require('./log.json')
+
 const bot = new Telebot(process.env.API_KEY)
+const LOG_DIR = './log.json'
+
 const ask = []
+const currentLog = _.clone(log)
 let admin = []
 
 bot.getChatAdministrators(process.env.CHAT_ID)
   .then(response => response.result.forEach(people => admin.push(people.user)))
   .catch(err => console.log(err))
 
+const stringify = (data) => JSON.stringify(data)
+
 function checkAdmin (user) {
   const isAdmin = _.find(admin, ['username', user])
   if (isAdmin) return isAdmin
   if (!isAdmin) bot.sendMessage(process.env.CHAT_ID, `You have no power here @${user}`)
+}
+
+function writeLog () {
+  fs.writeFile(LOG_DIR, stringify(currentLog), err => {
+    if (err) throw err
+  })
 }
 
 // Hello world!
@@ -26,9 +38,18 @@ bot.on(['audio', 'sticker'], msg => bot.deleteMessage(msg.chat.id, msg.message_i
 
 // Save photo & text post to .json log
 bot.on(['text', 'photo'], msg => {
-  if (msg.chat.id == process.env.CHAT_ID) {
-    log.push(msg)
-    fs.writeFile('./log.json', JSON.stringify(log), err => { if (err) throw err })
+  if (msg.chat.id === process.env.CHAT_ID) {
+    currentLog.push(msg)
+    writeLog()
+  }
+})
+
+// Replace log to edited text
+bot.on(['edit'], msg => {
+  if (msg.chat.id === process.env.CHAT_ID) {
+    const idx = _.findIndex(currentLog, { message_id: msg.message_id })
+    _.update(currentLog, idx, () => msg)
+    writeLog()
   }
 })
 
