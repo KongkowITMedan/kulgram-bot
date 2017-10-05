5ec91ba7ad76d7f62e5beebefe339663239f15da
ferrwan <ferrwanz@gmail.com>
2017-10-05 13:00:24 +0700

    add /start command to initialize chat setting

--- index.js
+++ index.js
@@ -10,18 +10,15 @@ const LOG_DIR = './log.json'
 
 const ask = []
 const currentLog = _.clone(log)
+let chatId
 let admin = []
-
-bot.getChatAdministrators(process.env.CHAT_ID)
-  .then(response => response.result.forEach(people => admin.push(people.user)))
-  .catch(err => console.log(err))
 
 const stringify = (data) => JSON.stringify(data)
 
 function checkAdmin (user) {
   const isAdmin = _.find(admin, ['username', user])
   if (isAdmin) return isAdmin
-  if (!isAdmin) bot.sendMessage(process.env.CHAT_ID, `You have no power here @${user}`)
+  if (!isAdmin) bot.sendMessage(chatId, `You have no power here @${user}`)
 }
 
 function writeLog () {
@@ -33,12 +30,23 @@ function writeLog () {
 // Hello world!
 bot.on(['/hello'], msg => msg.reply.text('Hello World!'))
 
+// On initialize chat
+bot.on('/start', msg => {
+  chatId = msg.chat.id
+  bot.getChatAdministrators(chatId)
+    .then(response => response.result.forEach(people => admin.push(people.user)))
+    .catch(err => console.log(err))
+  bot.getChat(chatId)
+    .then(response => console.log(response.result))
+    .catch(err => console.log(err))
+})
+
 // Delete audio & sticker post
 bot.on(['audio', 'sticker'], msg => bot.deleteMessage(msg.chat.id, msg.message_id))
 
 // Save photo & text post to .json log
 bot.on(['text', 'photo'], msg => {
-  if (msg.chat.id === process.env.CHAT_ID) {
+  if (msg.chat.id === chatId) {
     currentLog.push(msg)
     writeLog()
   }
@@ -46,7 +54,7 @@ bot.on(['text', 'photo'], msg => {
 
 // Replace log to edited text
 bot.on(['edit'], msg => {
-  if (msg.chat.id === process.env.CHAT_ID) {
+  if (msg.chat.id === chatId) {
     const idx = _.findIndex(currentLog, { message_id: msg.message_id })
     _.update(currentLog, idx, () => msg)
     writeLog()
